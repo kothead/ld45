8f08ac3594c017a88350a7db8612def8a705c6a5
kothead <stopkillingshoggoths@gmail.com>
2019-10-04 22:18:22 +0500

    Initial commit

---
+++ android/src/com/shoggoth/ld45/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.shoggoth.ld45;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.shoggoth.ld45.LD45;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD45(), config);
+	}
+}
---
+++ desktop/src/com/shoggoth/ld45/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shoggoth.ld45.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shoggoth.ld45.LD45;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD45(), config);
+	}
+}
